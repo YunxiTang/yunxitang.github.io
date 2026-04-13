2ce2595d7ae882c8253ef1074a84af59913f4f6e
YunxiTang <54171095+YunxiTang@users.noreply.github.com>
2026-04-13 10:12:49 +0000

    Deploying to gh-pages from @ YunxiTang/yunxitang.github.io@a4292012fcd6b9ad1963051f45790ddf533b85cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-successfully-passed-my-ph-d-defense-at-the-chinese-university-of-hong-kong-thesis-learning-based-deformable-linear-object-manipulation-in-unstructured-environments",
+          title: 'Successfully passed my Ph.D. defense at The Chinese University of Hong Kong. Thesis:...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
